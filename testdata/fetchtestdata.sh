50ae5e85d11290f69b0acc00b93342d19fb0bd0a
Jouni Ahto <jahto@welho.com>
2018-03-25 19:33:15 +0300

    Fixed .gitignore, removed one accidentally committed testfile and fetched a lot more of testdata for the next development phase.

--- testdata/fetchtestdata.sh
+++ testdata/fetchtestdata.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-for num in {0..9};
+for num in {0..999};
 do
 curl http://api.digitransit.fi/realtime/vehicle-positions/v1/siriaccess/vm/json > "exampledata"$num".json";
 # Assuming it takes approximately one second to fetch the data, so we get
-# something like one sample per minute.
-sleep 59;
+# something like one sample per 10 seconds.
+sleep 9;
 done
