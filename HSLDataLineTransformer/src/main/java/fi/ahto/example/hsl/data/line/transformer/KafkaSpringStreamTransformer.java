b729af8f1383ce6d51273ab693b7f8657d7a0ca4
Jouni Ahto <jahto@welho.com>
2018-03-26 11:53:06 +0300

    Corrected a misspelling.

--- HSLDataLineTransformer/src/main/java/fi/ahto/example/hsl/data/line/transformer/KafkaSpringStreamTransformer.java
+++ HSLDataLineTransformer/src/main/java/fi/ahto/example/hsl/data/line/transformer/KafkaSpringStreamTransformer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fi.ahto.example.hsl.data.line.transfomer;
+package fi.ahto.example.hsl.data.line.transformer;
 
 
 import org.springframework.boot.SpringApplication;
--- HSLDataLineTransformer/src/main/java/fi/ahto/example/hsl/data/line/transformer/LineTransformer.java
+++ HSLDataLineTransformer/src/main/java/fi/ahto/example/hsl/data/line/transformer/LineTransformer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fi.ahto.example.hsl.data.line.transfomer;
+package fi.ahto.example.hsl.data.line.transformer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.ahto.example.hsl.data.contracts.siri.VehicleActivityFlattened;
